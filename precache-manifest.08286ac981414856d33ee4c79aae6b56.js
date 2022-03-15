ce5002dc095484eb3b5a3daf38d3a24d880f0c23
James Harbridge <jamesharbridge@outlook.com>
2022-03-15 08:32:27 +0000

    Updates

---
+++ precache-manifest.08286ac981414856d33ee4c79aae6b56.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "41cb2a563c618f45324c",
+    "url": "/token-staking/static/css/main.771b4fd3.chunk.css"
+  },
+  {
+    "revision": "41cb2a563c618f45324c",
+    "url": "/token-staking/static/js/main.41cb2a56.chunk.js"
+  },
+  {
+    "revision": "7669e0d51ba42693407c",
+    "url": "/token-staking/static/js/1.7669e0d5.chunk.js"
+  },
+  {
+    "revision": "21928bd966c96b68fb1b",
+    "url": "/token-staking/static/js/runtime~main.21928bd9.js"
+  },
+  {
+    "revision": "c05c4481ac1645632bb4ddfae94f8298",
+    "url": "/token-staking/static/media/CoverImage.c05c4481.svg"
+  },
+  {
+    "revision": "21cb4e702a03ce2e7f2a17d1d8718864",
+    "url": "/token-staking/static/media/NewsSVG.21cb4e70.svg"
+  },
+  {
+    "revision": "726ff9c0c5c98641bf3a5ed1c3c3bf3f",
+    "url": "/token-staking/index.html"
+  }
+];
